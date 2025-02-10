921d6cc0920a8e7ae1cda6497bf4940e2fb58170
Yaroslav <kashuliy@tcd.ie>
2025-02-10 13:25:05 +0000

    created unified requirements and venv

--- config.py
+++ config.py
@@ -6,20 +6,28 @@ from dotenv import load_dotenv
 
 @dataclass
 class Config:
+    zap_path: str
+    zap_host: str
+    zap_port: str
     zap_api: str
-    
+
 
 def get_config():
     load_dotenv()
 
-    ZAP_PATh = "/System/Volumes/Data/Applications/ZAP.app/Contents/Java/zap.sh"  # adjust this to your ZAP launch script
-    ZAP_HOST = "localhost"
-    ZAP_PORT = "8080"
-    token = os.getenv("ZAP_API_KEY", "")
+    path = "/System/Volumes/Data/Applications/ZAP.app/Contents/Java/zap.sh"  # adjust this to your ZAP launch script
+    host = "localhost"
+    port = "8080"
+    token = os.getenv("ZAP_API_KEY", " ")
     if not token:
         raise ValueError("ZAP_API_KEY environment variable is missing!")
 
-    return Config(zap_api=token)
+    return Config(
+                zap_path = path,
+                zap_host = host,
+                zap_port = port,
+                zap_api  = zap_token
+        )
 
 # Load the configuration
 config = get_config()
