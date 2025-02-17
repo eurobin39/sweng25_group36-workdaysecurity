7dd2f21cd8389f3e8a071c27ab24aba0fb9b7eb3
aalaa <aamohamm@tcd.ie>
2025-02-17 20:55:22 +0000

    Nav bar + logo added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,15 +2,22 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image"; 
+import NavBar from "../components/ui/NavBar"; // Ensure the casing matches the file name
 
 export default function HomePage() {
   const router = useRouter();
 
   return (
+    <div>
+    <NavBar />
+
+
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-blue-600 to-blue-300">
-      <h1 className="text-white text-3xl md:text-4xl font-bold mb-6">
+      <h1 className="text-white text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-400 drop-shadow-lg animate-bounce">
         What is your role?
       </h1>
+      
       <div className="flex space-x-6">
         <button
           className="bg-gradient-to-r from-orange-400 to-orange-600 hover:from-orange-500 hover:to-orange-700 text-white font-bold py-3 px-6 rounded-lg text-lg shadow-lg transition-all"
@@ -26,5 +33,7 @@ export default function HomePage() {
         </button>
       </div>
     </div>
+    </div>
+    
   );
 }
